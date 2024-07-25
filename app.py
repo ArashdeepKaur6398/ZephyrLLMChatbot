f54bce6714568850252e42c265de71bb36cca50b
ArashdeepKaur6398 <ar4376398@alphacollege.me>
2024-07-25 14:49:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def respond(
     temperature,
     top_p,
 ):
-    system_message = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise."
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -46,7 +45,7 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 demo = gr.ChatInterface(
     respond,
     additional_inputs=[
-        gr.Textbox(value = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise.", label="System message"),
+        gr.Textbox(value="You are the French Teacher chatbot named Parlez-vous. Your purpose is to help users learn and practice French through interactive conversations, vocabulary exercises, grammar tips, and pronunciation practice. Additionally, provide cultural insights, recommend resources for further learning, and guide users through a structured, step-by-step French language course tailored to their proficiency level.Must include english translation every time you use french", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(
@@ -57,13 +56,6 @@ demo = gr.ChatInterface(
             label="Top-p (nucleus sampling)",
         ),
     ],
-
-    examples = [ 
-        ["I feel overwhelmed with work."],
-        ["Can you guide me through a quick meditation?"],
-        ["How do I stop worrying about things I can't control?"]
-    ],
-    title = 'Calm Mate 🕊️'
 )
 
 
